69c01b1c94a7b6fb0e005de3338f1fbd400d8227
Babak <babak.gholamzadeh92@gmail.com>
2019-07-02 00:00:40 +0430

    APIs Completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,111 @@
 const User = require('../models/user');
+const validate = require('../lib/validation');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // The object controller that's going to be exported
 const authController = {};
 
 // Register new user
 authController.register = async (req, res) => {
-  res.send('register page');
+  try {
+    // Exract the require fields form request body
+    let {
+      name,
+      email,
+      password
+    } = req.body;
+    // Validate name field, Not be null or empty
+    validate(name).isEmpty('The name field is required!');
+    // Validate email field, Not be null or empty
+    validate(email).isEmpty('The email field is required!');
+    // Validate password field, Not be null or empty, and be at least 3 characters
+    validate(password).isEmpty('The password field is required!').lenLessThan(3, 'The password must be at least 3 chatacters!');
+
+    // Hash the password
+    password = bcrypt.hashSync(password, 10);
+
+    // If everything is fine then create a new user
+    const user = new User({
+      name,
+      email,
+      password
+    });
+
+    // Add the user to DB
+    await user.save();
+
+    // Avoid to send back the password
+    user.password = undefined;
+
+    // Send back the created user
+    res.json(user);
+
+  } catch (err) {
+    res.status(400).json({
+      error: err.message
+    });
+  }
 };
 
 // Login to account
 authController.login = async (req, res) => {
-  res.send('login page');
+  try {
+    // Exract the require fields form request body
+    let {
+      email,
+      password
+    } = req.body;
+    // Validate email field, Not be null or empty
+    validate(email).isEmpty('The email field is required!');
+    // Validate password field, Not be null or empty
+    validate(password).isEmpty('The password field is required!');
+
+    // Check the user exists
+    const user = await User.findOne({ email });
+    // If the user doesn't exists, throw an error
+    if(!user) {
+      throw new Error('The username or/and password is incorrect!');
+    }
+    // If the password is incorrect, throw an error
+    if(!bcrypt.compareSync(password, user.password)) {
+      throw new Error('The username or/and password is incorrect!');
+    }
+    // If username and password are valid, send back a token for authorization the user
+    res.json({
+      token: jwt.sign({ name: user.name, email: user.email, _id: user.id }, 'SecretAPIKey')
+    });
+
+  } catch (err) {
+    res.status(401).json({
+      error: err.message
+    });
+  }
 };
 
 // Authorize the user
+// This function is used as middleware
 authController.auth = async (req, res, next) => {
-  // res.send('register page');
+  // For authorization just check if the valid token exists in request header
+  // If it does, then attach the user object to request object
+  // Else req.user = undefined
+  try {
+    if(req.headers.token) {
+      // If the token is valid, the decode will equal to payload
+      const decode = jwt.verify(req.headers.token, 'SecretAPIKey');
+      // Check if the user still exists in DB
+      const user = await User.findOne({ email: decode.email }).select('-password');
+      req.user = user;
+    }
+    else {
+      req.user = undefined;
+    }
+  } catch (err) {
+    req.user = undefined;
+  } finally {
+    next();
+  }
 };
 
+// Export controller object
 module.exports = authController;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const connectDB = require('./models/config');
+const { auth } = require('./controllers/auth');
 
 // Connect to DB
 connectDB();
@@ -8,18 +10,30 @@ connectDB();
 // Use port 3000 if there isn't any port sat
 const PORT = process.env.PORT || 3000;
 
+// Parse the data in request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// The Authorization can be used for all end points or just some of them
+// app.use(auth);
+
 // Define the routers as middleware to controll the routes
 // Register Route
 app.use('/register', require('./routes/register'));
 // Login Route
 app.use('/login', require('./routes/login'));
 
-
-app.get('/', (req, res) => {
-  res.send('Home');
+// Home Route
+app.get('/', auth, (req, res) => {
+  if(req.user) {
+    res.send('The user authorized, and can access to this page!');
+  }
+  else {
+    res.send('Access to this page has been denied!');
+  }
 });
 
-
+// Server listen to the port and waiting for a request
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
