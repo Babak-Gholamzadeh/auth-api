abb9ba379c56129e4946c1d36a34bb4ff60820c0
Babak <babak.gholamzadeh92@gmail.com>
2019-07-02 00:38:08 +0430

    fixed bugs

--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,7 @@ const { auth } = require('../controllers/auth');
 // @desc   Access to home API
 // @access Private
 router.get('/', auth, (req, res) => {
+  // If the object user defined in request, means the user is authorized
   if(req.user) {
     res.send('The user authorized, and can access to this page!');
   }
